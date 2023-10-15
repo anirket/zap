24ca67adbfc3c73888febbc5d30fc20e41665299
Aniket <aniket.kulkarni@smallcase.com>
2023-10-15 23:08:57 +0530

    fix redirect URL

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Auth0Provider } from '@auth0/auth0-react';
 ReactDOM.render(
     <Auth0Provider domain= {process.env.REACT_APP_DOMAIN}
         clientId={process.env.REACT_APP_CLIENTID}
-        redirectUri={"http://localhost:3000/"}>
+        redirectUri={window.location.origin}>
         <App />
     </Auth0Provider>
     , document.getElementById("root")
